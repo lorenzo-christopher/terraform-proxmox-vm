c05b6804adc78b1c8a6707b2d2bf7c583285222b
lorenzo-christopher <lorenzo.odlare.christopher@gmail.com>
2026-06-05 17:20:36 -0500

    comment out output values

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,18 @@
 
-output "vm_id" {
-  description = "The ID of the created VM"
-  value       = proxmox_virtual_environment_vm.vm.id
-}
+# output "vm_id" {
+#   description = "The ID of the created VM"
+#   value       = proxmox_virtual_environment_vm.vm.id
+# }
 
-output "vm_name" {
-  description = "The name of the created VM"
-  value       = proxmox_virtual_environment_vm.vm.name
-}
+# output "vm_name" {
+#   description = "The name of the created VM"
+#   value       = proxmox_virtual_environment_vm.vm.name
+# }
 
-output "vm_ipv4_address" {
-  description = "Instance IPv4 address"
-  value       = proxmox_virtual_environment_vm.vm.ipv4_addresses[1][0]
-}
+# output "vm_ipv4_address" {
+#   description = "Instance IPv4 address"
+#   value       = proxmox_virtual_environment_vm.vm.ipv4_addresses[1][0]
+# }
 
 # output "vm_ipv6_addresses" {
 #   description = "The IPv6 addresses of the VM"
